0f9d1f6699b1dc67e41bac4c5a3e77bc9e35fb0c
Jérôme Garo <jerome.garo@ima.it>
2021-02-01 17:45:58 +0100

    Modif Clé chef département + Création seed

--- ProjetTimbrage/database/migrations/2021_01_27_153538_create_departments_table.php
+++ ProjetTimbrage/database/migrations/2021_01_27_153538_create_departments_table.php
@@ -15,7 +15,7 @@ class CreateDepartmentsTable extends Migration
     {
         Schema::create('departments', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->nullable()->constrained();
             $table->string('name');
             $table->timestamps();
         });
--- ProjetTimbrage/database/seeders/DatabaseSeeder.php
+++ ProjetTimbrage/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Department;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +17,82 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      // \App\Models\User::factory(10)->create();
+
+      Department::create([
+        'user_id' => null,
+        'name' => 'Soft',
+      ]);
+
+      Department::create([
+        'user_id' => null,
+        'name' => 'Mechanics',
+      ]);
+
+      Department::create([
+        'user_id' => null,
+        'name' => 'Human Resouces',
+      ]);
+
+      Department::create([
+        'user_id' => null,
+        'name' => 'Purchase',
+      ]);
+
+      User::create([
+        'department_id' => 1,
+        'firstName' => 'Jérôme',
+        'lastName' => 'Garo',
+        'email' => 'jerome.garo@gmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('password'),
+        'remember_token' => Str::random(10),
+        'grade' => 100,
+      ]);
+
+      User::create([
+        'department_id' => 1,
+        'firstName' => 'Luca',
+        'lastName' => 'Rigazzi',
+        'email' => 'luca.riggazi@gmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('password'),
+        'remember_token' => Str::random(10),
+        'grade' => 100,
+      ]);
+
+      User::create([
+        'department_id' => 3,
+        'firstName' => 'Human',
+        'lastName' => 'Resources',
+        'email' => 'hr@gmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('password'),
+        'remember_token' => Str::random(10),
+        'grade' => 1000,
+      ]);
+
+      User::create([
+        'department_id' => 2,
+        'firstName' => 'J-P',
+        'lastName' => 'Meca',
+        'email' => 'jp.meca@gmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('password'),
+        'remember_token' => Str::random(10),
+        'grade' => 200,
+      ]);
+
+      User::create([
+        'department_id' => 4,
+        'firstName' => 'J-F',
+        'lastName' => 'Purchase',
+        'email' => 'jf.purchase@gmail.com',
+        'email_verified_at' => now(),
+        'password' => Hash::make('password'),
+        'remember_token' => Str::random(10),
+        'grade' => 300,
+      ]);
+
     }
 }
