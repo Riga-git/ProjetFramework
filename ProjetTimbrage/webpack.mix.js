947322d0877d24a7f3f7265fffc1fab48cc0486e
Jérôme Garo <jerome.garo@gmail.com>
2021-02-10 00:16:15 +0100

    Suppression de tous les logos

--- ProjetTimbrage/webpack.mix.js
+++ ProjetTimbrage/webpack.mix.js
@@ -14,10 +14,12 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js').vue()
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
-        require('tailwindcss'),
+        //require('tailwindcss'),
         require('autoprefixer'),
     ])
     .webpackConfig(require('./webpack.config'));
+
+mix.sass('resources/sass/app.scss', 'public/css');
 
 if (mix.inProduction()) {
     mix.version();
