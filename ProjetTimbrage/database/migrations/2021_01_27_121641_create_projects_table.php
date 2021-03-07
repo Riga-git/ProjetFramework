8efb9e449feaf20609f74f9087cfa30d9c2c7d8d
Jérôme Garo <jerome.garo@gmail.com>
2021-03-07 12:58:08 +0100

    Ajout colonne softDeletes dans projets

--- ProjetTimbrage/database/migrations/2021_01_27_121641_create_projects_table.php
+++ ProjetTimbrage/database/migrations/2021_01_27_121641_create_projects_table.php
@@ -17,6 +17,7 @@ class CreateProjectsTable extends Migration
             $table->id();
             $table->string('number');
             $table->string('name');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
