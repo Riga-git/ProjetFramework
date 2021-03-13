b792d11bc43360e81a9dba1b1acd00c58469bed8
Lri <luca.rigazzi@gmail.com>
2021-03-13 16:26:48 +0100

    HomaPage.vue comme root

--- ProjetTimbrage/resources/js/Pages/HomePage.vue
+++ ProjetTimbrage/resources/js/Pages/HomePage.vue
@@ -1,22 +1,57 @@
 <template>
-    <div>HomePage</div>
+    <app-layout>
+        <template #header>
+        </template>
+    <div class="container mt-5">
+        <div class="columns is-2 ">
+            <div class="column is-half is-justify-content-space-around is-align-items-center">
+                <div class="box">
+                    <div class="columns is-centered">
+                        <div class="column is-half">
+                            <figure class="image is-64x64" style="margin:auto">
+                                <img class="block" src="/Icons/ClockIn.png" alt="Clock In">
+                            </figure>
+                        </div>
+                        <div class="column is-half">
+                            <figure class="image is-64x64" style="margin:auto">
+                                <img class="block" src="/Icons/Clockings.png" alt="Clockings">
+                            </figure>
+                        </div>
+                    </div>
+                    <div class="columns is-justify-content-space-around is-align-items-center">
+                        <div class="column is-half">
+                            <figure class="image is-64x64" style="margin:auto">
+                                <img src="/Icons/Projects.png" alt="Clock In">
+                            </figure>
+                        </div>
+                        <div class="column is-half">
+                            <figure class="image is-64x64" style="margin:auto">
+                                <img src="/Icons/Assignments.png" alt="Clockings">
+                            </figure>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="column is-half">
+                <div class="box">
+                  <current-clockings :date="null"></current-clockings>
+                </div>
+            </div>
+        </div>
+    </div>
+    </app-layout>
 </template>
 
 <script>
-  //import JetNavLink from '@/Jetstream/NavLink'
+    import AppLayout from '@/Layouts/AppLayout'
+    import CurrentClockings from '@/Components/CurrentClockings'
 
-  export default {
-    //props: ['departments'],
+    export default {
+        components: {
+            AppLayout,
+            CurrentClockings
+        },
 
-    /*components:{
-      JetNavLink
-    },*/
-
-    data() {
-      return {
-        
-      }
-    },
-  }
-
+    
+    }
 </script>
